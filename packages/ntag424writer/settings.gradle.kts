04ae5a9e8a01a5ecf167cf1d7a93576bf9f123ba
Aplulu <aplulu.liv@gmail.com>
2024-06-17 01:19:04 +0900

    :sparkles: Add NFC Writer Android App

---
+++ packages/ntag424writer/settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://raw.githubusercontent.com/johnnyb/ntag424-java/mvn-repo/")
+        }
+    }
+}
+
+rootProject.name = "NTAG 424 Writer"
+include(":app")
